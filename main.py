4c5ce3b5144d0971c6a7c4b4ad543cddee98569c
Renato Byrro <rmbyrro@gmail.com>
2020-08-03 16:29:47 -0300

    Update imports to absolute

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from module_hack import hack
-from module_mock import whatever
-
-
-print('\nHACK:')
-print(hack)
-print(hack.attr)
-print(hack.method())
-
-print('\nMOCK:')
-print(whatever)
-print(whatever.attr)
-print(whatever.method())
-
 '''
     Conclusion: seems that using the 'Hack' version is better, since it doesn't
     trigger syntax/linter errors (happens in the 'Mock' version).
 
     The 'Hack' version can't emulate an entire module, but it should not be a
-    problem to 
+    problem.
 '''
+
+import module_hack
+import module_mock
+
+
+print('\nHACK:')
+print(module_hack.hack)
+print(module_hack.hack.attr)
+print(module_hack.hack.method())
+
+print('\nMOCK:')
+print(module_mock.whatever)
+print(module_mock.whatever.attr)
+print(module_mock.whatever.method())
